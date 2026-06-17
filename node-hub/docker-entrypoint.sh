0796357acc98d6c703084a53b653bf27281a14b1
Ville Heikkiniemi <ville.heikkiniemi@centria.fi>
2026-06-17 12:49:59 +0300

    update broker auth + more

--- node-hub/docker-entrypoint.sh
+++ node-hub/docker-entrypoint.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 set -e
 
-envsubst < /mosquitto/config/mosquitto.conf.template \
-         > /mosquitto/config/mosquitto.conf
+cp /mosquitto/config/mosquitto.conf.template /mosquitto/config/mosquitto.conf
+
+sed -i "s|\${PGHOST}|${PGHOST}|g" /mosquitto/config/mosquitto.conf
+sed -i "s|\${PGPORT}|${PGPORT}|g" /mosquitto/config/mosquitto.conf
+sed -i "s|\${PGDATABASE}|${PGDATABASE}|g" /mosquitto/config/mosquitto.conf
+sed -i "s|\${PGUSER}|${PGUSER}|g" /mosquitto/config/mosquitto.conf
+sed -i "s|\${PGPASSWORD}|${PGPASSWORD}|g" /mosquitto/config/mosquitto.conf
 
 exec mosquitto -c /mosquitto/config/mosquitto.conf
