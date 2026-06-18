d094ecde734fa0b751d32a7811be5fe629bb027e
Ville Heikkiniemi <ville.heikkiniemi@centria.fi>
2026-06-18 12:33:04 +0300

    update mosquitto dyn sec

--- node-hub/init-dynsec.sh
+++ node-hub/init-dynsec.sh
@@ -8,17 +8,15 @@ ADMIN_PASS="${DYNSEC_ADMIN_PASS:?DYNSEC_ADMIN_PASS is missing}"
 : "${INGESTORPASS:?INGESTORPASS is missing}"
 
 echo "Creating ingestor role..."
-mosquitto_ctrl -h localhost -u "$ADMIN_USER" -P "$ADMIN_PASS" dynsec createRole ingestor-role || true  >/dev/null 2>&1;
+mosquitto_ctrl -h localhost -u "$ADMIN_USER" -P "$ADMIN_PASS" dynsec createRole ingestor-role || true
 
-echo "Adding ACLs..."
-mosquitto_ctrl -h localhost -u "$ADMIN_USER" -P "$ADMIN_PASS" dynsec addRoleACL ingestor-role subscribePattern "devices/#" allow 10 || true  >/dev/null 2>&1;
-mosquitto_ctrl -h localhost -u "$ADMIN_USER" -P "$ADMIN_PASS" dynsec addRoleACL ingestor-role publishClientReceive "devices/#" allow 10 || true  >/dev/null 2>&1;
+echo "Adding ingestor ACLs..."
+mosquitto_ctrl -h localhost -u "$ADMIN_USER" -P "$ADMIN_PASS" dynsec addRoleACL ingestor-role subscribePattern "devices/#" allow 10 || true
+mosquitto_ctrl -h localhost -u "$ADMIN_USER" -P "$ADMIN_PASS" dynsec addRoleACL ingestor-role publishClientReceive "devices/#" allow 10 || true 
+mosquitto_ctrl -h localhost -u "$ADMIN_USER" -P "$ADMIN_PASS" dynsec addRoleACL ingestor-role subscribePattern '$SYS/#' allow 10 || true
+mosquitto_ctrl -h localhost -u "$ADMIN_USER" -P "$ADMIN_PASS" dynsec addRoleACL ingestor-role publishClientReceive '$SYS/#' allow 10 || true
 
 echo "Creating ingestor client..."
-#if ! mosquitto_ctrl -h localhost -u "$ADMIN_USER" -P "$ADMIN_PASS" dynsec getClient "$INGESTORUSER" >/dev/null 2>&1; then
-#printf '\n' | mosquitto_ctrl -h localhost -u "$ADMIN_USER" -P "$ADMIN_PASS" dynsec createClient "$INGESTORUSER"
-#fi
-
 if ! mosquitto_ctrl -h localhost -u "$ADMIN_USER" -P "$ADMIN_PASS" dynsec listClients | grep -qx "$INGESTORUSER"; then
     printf '\n' | mosquitto_ctrl -h localhost -u "$ADMIN_USER" -P "$ADMIN_PASS" dynsec createClient "$INGESTORUSER"
 else
@@ -28,5 +26,16 @@ fi
 mosquitto_ctrl -h localhost -u "$ADMIN_USER" -P "$ADMIN_PASS" dynsec setClientPassword "$INGESTORUSER" "$INGESTORPASS"
 mosquitto_ctrl -h localhost -u "$ADMIN_USER" -P "$ADMIN_PASS" dynsec setClientId "$INGESTORUSER" "$INGESTORUSER"
 mosquitto_ctrl -h localhost -u "$ADMIN_USER" -P "$ADMIN_PASS" dynsec addClientRole "$INGESTORUSER" ingestor-role 10 || true
+
+echo "Creating anonymous device role..."
+mosquitto_ctrl -h localhost -u "$ADMIN_USER" -P "$ADMIN_PASS" dynsec createRole anonymous-device-role || true
+
+echo "Adding anonymous device ACLs..."
+mosquitto_ctrl -h localhost -u "$ADMIN_USER" -P "$ADMIN_PASS" dynsec addRoleACL anonymous-device-role publishClientSend "devices/#" allow 10 || true
+mosquitto_ctrl -h localhost -u "$ADMIN_USER" -P "$ADMIN_PASS" dynsec addRoleACL anonymous-device-role subscribePattern "devices/#" allow 10 || true
+mosquitto_ctrl -h localhost -u "$ADMIN_USER" -P "$ADMIN_PASS" dynsec addRoleACL anonymous-device-role publishClientReceive "devices/#" allow 10 || true
+
+echo "Adding anonymous device role to unauthenticated group..."
+mosquitto_ctrl -h localhost -u "$ADMIN_USER" -P "$ADMIN_PASS" dynsec addGroupRole unauthenticated anonymous-device-role 10 || true
 
 echo "Done."
