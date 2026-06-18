cfdfc88a80e772b6dd9a21a59f859063648018e2
Ville Heikkiniemi <ville.heikkiniemi@centria.fi>
2026-06-18 19:12:55 +0300

    update mosquitto dyn sec

--- node-hub/init-dynsec.sh
+++ node-hub/init-dynsec.sh
@@ -27,18 +27,29 @@ mosquitto_ctrl -h localhost -u "$ADMIN_USER" -P "$ADMIN_PASS" dynsec setClientPa
 mosquitto_ctrl -h localhost -u "$ADMIN_USER" -P "$ADMIN_PASS" dynsec setClientId "$INGESTORUSER" "$INGESTORUSER"
 mosquitto_ctrl -h localhost -u "$ADMIN_USER" -P "$ADMIN_PASS" dynsec addClientRole "$INGESTORUSER" ingestor-role 10 || true
 
-echo "Creating anonymous device role..."
-mosquitto_ctrl -h localhost -u "$ADMIN_USER" -P "$ADMIN_PASS" dynsec createRole anonymous-device-role || true
+#echo "Creating anonymous device role..."
+#mosquitto_ctrl -h localhost -u "$ADMIN_USER" -P "$ADMIN_PASS" dynsec createRole anonymous-device-role || true
 
-echo "Adding anonymous device ACLs..."
-mosquitto_ctrl -h localhost -u "$ADMIN_USER" -P "$ADMIN_PASS" dynsec addRoleACL anonymous-device-role publishClientSend "devices/#" allow 10 || true
-mosquitto_ctrl -h localhost -u "$ADMIN_USER" -P "$ADMIN_PASS" dynsec addRoleACL anonymous-device-role subscribePattern "devices/#" allow 10 || true
-mosquitto_ctrl -h localhost -u "$ADMIN_USER" -P "$ADMIN_PASS" dynsec addRoleACL anonymous-device-role publishClientReceive "devices/#" allow 10 || true
+#echo "Adding anonymous device ACLs..."
+echo "Configure the Anonymous Group"
+mosquitto_ctrl -h localhost -u "$ADMIN_USER" -P "$ADMIN_PASS" dynsec createRole device_anonymous_role
+echo "Allow anonymous users to publish to device topics"
+mosquitto_ctrl -h localhost -u "$ADMIN_USER" -P "$ADMIN_PASS" dynsec addRoleACL device_anonymous_role publishClientSend devices/# allow
+echo "Allow anonymous users to subscribe to device topics"
+mosquitto_ctrl -h localhost -u "$ADMIN_USER" -P "$ADMIN_PASS" dynsec addRoleACL device_anonymous_role subscribe devices/# allow
+echo "Attach the Role to the Anonymous Group"
+mosquitto_ctrl -h localhost -u "$ADMIN_USER" -P "$ADMIN_PASS" dynsec addGroupRole anonymous device_anonymous_role
+echo "Explicitly Set the Anonymous Group"
+mosquitto_ctrl -h localhost -u "$ADMIN_USER" -P "$ADMIN_PASS" dynsec setAnonymousGroup anonymous
 
-echo "Creating unauthenticated group..."
-mosquitto_ctrl -h localhost -u "$ADMIN_USER" -P "$ADMIN_PASS" dynsec createGroup unauthenticated || true
+#mosquitto_ctrl -h localhost -u "$ADMIN_USER" -P "$ADMIN_PASS" dynsec addRoleACL anonymous-device-role publishClientSend "devices/#" allow 10 || true
+#mosquitto_ctrl -h localhost -u "$ADMIN_USER" -P "$ADMIN_PASS" dynsec addRoleACL anonymous-device-role subscribePattern "devices/#" allow 10 || true
+#mosquitto_ctrl -h localhost -u "$ADMIN_USER" -P "$ADMIN_PASS" dynsec addRoleACL anonymous-device-role publishClientReceive "devices/#" allow 10 || true
 
-echo "Adding anonymous device role to unauthenticated group..."
-mosquitto_ctrl -h localhost -u "$ADMIN_USER" -P "$ADMIN_PASS" dynsec addGroupRole unauthenticated anonymous-device-role 10 || true
+#echo "Creating unauthenticated group..."
+#mosquitto_ctrl -h localhost -u "$ADMIN_USER" -P "$ADMIN_PASS" dynsec createGroup unauthenticated || true
+
+#echo "Adding anonymous device role to unauthenticated group..."
+#mosquitto_ctrl -h localhost -u "$ADMIN_USER" -P "$ADMIN_PASS" dynsec addGroupRole unauthenticated anonymous-device-role 10 || true
 
 echo "Done."
